317507b718fb333f05ed1e787e41c77c3dd83e57
Gavin <gavinbrrtt@gmail.com>
2020-10-26 11:20:18 -0700

    Almost fixed mnemonic generation bugs.

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from src.entropy import *
 from src.seeds import gather_words
 from hashlib import pbkdf2_hmac
-from binascii import hexlify
-import unicodedata
 
+# generate entropy
 #entropy = generate()
+ent_size = 32
 
-entropy = b'\xff' * 32
+entropy = b'\x00' * 32
 
+# generate the checksum length
+length = get_length(ent_size)
+
+# generate the SHA-256 digest of the entropy
 digest = sha_hash(entropy)
 
-print(digest)
+# extract the checksum from the digest
+check = checksum(digest, length)
 
-check = checksum(entropy, digest)
-
+# append checksum to the entropy
 concat = concat_checksum(entropy, check);
 
-splits = split_digest(bytes_to_int(concat))
+# split buffer into groups of 11
+splits = split_digest(bytes_to_int(concat), ent_size)
 
+'''
+# extract words from mnemonic word file
 words = gather_words()
+
 
 x = [words[s] for s in splits]
 
@@ -28,4 +36,5 @@ salt = "mnemonic"
 hsh = pbkdf2_hmac('sha512', seed.encode('utf-8'), salt.encode('utf-8'), 2048, 64)
 
 print(seed)
-print(hexlify(hsh))
+#print(hexlify(hsh))
+'''
--- src/entropy.py
+++ src/entropy.py
@@ -1,9 +1,10 @@
 from os import urandom
 from hashlib import sha256
 from sys import byteorder
+from binascii import hexlify, unhexlify
 
 def generate():
-	# return 256 bits of entropy
+	# return bits of entropy - 16, 20, 24, 28, 32
 	return urandom(32)
 
 def sha_hash(entropy):
@@ -13,23 +14,31 @@ def sha_hash(entropy):
 	return hasher.digest()
 
 def bytes_to_int(digest):
+	# convert bytes to integer
 	return int.from_bytes(digest, byteorder)
 
-def checksum(entropy, digest):
-	# return a checksum of our entropy
-	integer = bytes_to_int(entropy)
-	length = integer.bit_length()
-	sumlength = length // 32
-	print(f'length: {length}')
-	print(f'sumlength: {sumlength}')
-	num = integer >> (length - sumlength)
-	print(num)
-	return num.to_bytes(sumlength, byteorder)
+def get_length(ent_size):
+	# compute the length of the checksum
+	return (ent_size * 8) // 32
+
+def pad_hex(x):
+	# pad any hex nibbles
+	if len(x) % 2:
+		return b'0' + x
+	return x
+
+def checksum(digest, length):
+	# turn hex into readable stream
+	hex_hash = hexlify(digest)
+	x = pad_hex(hex_hash[:length//4])
+	return unhexlify(x)
 
 def concat_checksum(entropy, checksum):
 	# concatenate the checksum to the end of the entropy
-	return entropy + checksum
+	return checksum + entropy[::-1]
 
-def split_digest(digest):
+def split_digest(digest, ent_length):
 	# split digest up into 24 11-bit numbers
-	return [(digest >> idx) & 0x07ff for idx in range(0, digest.bit_length(), 11)]
+	return [(digest >> idx) & 0x07ff for idx in range(0, ent_length * 8, 11)][::-1]
+
+
