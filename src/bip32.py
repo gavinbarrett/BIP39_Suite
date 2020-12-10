85f53d59418532295af219d9b45502dfa41638f1
Gavin <gavinbrrtt@gmail.com>
2020-12-10 15:45:45 -0800

    Added secp256k1 class and associated parameters.

--- src/bip32.py
+++ src/bip32.py
@@ -1,6 +1,19 @@
 from os import urandom
 
-def generate_chain():
+class secp256k1():
+	# This class represents the secp256k1 elliptic curve: y^2 = x^3 + b (mod p)
+	# Parameters sourced from https://en.bitcoin.it/wiki/Secp256k1
+	def __init__(self):
+		self.p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
+		self.b = 0x0000000000000000000000000000000000000000000000000000000000000007
+		self.x = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
+		self.y = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
+		self.n = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+
+def point_mult(p):
+	# FIXME: perform EC point addition with secp256k1 parameters
+	pass
+
+def generate_chaincode():
 	# return random 256-bit chain code
 	return urandom(32)
-
