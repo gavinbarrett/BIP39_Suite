f7068ae2e3bb2463494f4c8b8e79a79a1999eec6
Gavin <gavinbrrtt@gmail.com>
2020-12-10 22:52:54 -0800

    Added BIP32 helper functions.

--- src/bip32.py
+++ src/bip32.py
@@ -1,4 +1,6 @@
-from os import urandom
+from hashlib import pbkdf2_hmac
+from binascii import hexlify, unhexlify
+from bip39 import bip39, generate_rootseed
 
 class secp256k1():
 	# This class represents the secp256k1 elliptic curve: y^2 = x^3 + b (mod p)
@@ -12,8 +14,31 @@ class secp256k1():
 
 def point_mult(p):
 	# FIXME: perform EC point addition with secp256k1 parameters
+	curve = secp256k1()
+	K = 0
+	for i in range(p):
+		K += (curve.b) % curve.n
+
+def generate_rootkey(seed):
+	# derive the root key from the BIP39 seed
+	return hexlify(pbkdf2_hmac('sha512', seed, b'Bitcoin seed', 2048, 64))
+
+def split_rootkey(rootkey):
+	# split the root key into a master secret key and a master chain code
+	return rootkey[:len(rootkey)//2], rootkey[len(rootkey)//2:]
+
+def generate_child_privkey():
 	pass
 
-def generate_chaincode():
-	# return random 256-bit chain code
-	return urandom(32)
+def generate_child_pubkey():
+	pass
+
+def serialize_extended_key():
+	pass
+
+
+
+if __name__ == "__main__":
+	seed = generate_rootseed('bike genius install embrace jewel salute ice tenant patch aspect police toddler', '')
+	print(seed)
+
