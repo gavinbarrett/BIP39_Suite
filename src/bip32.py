ff8fe1985a4dae7c9c059617a39fb162105d417a
Gavin <gavinbrrtt@gmail.com>
2020-12-13 13:21:52 -0800

    Finished API for secp256k1 elliptic curve multiplication. The pubkey value generated is not correct.

--- src/bip32.py
+++ src/bip32.py
@@ -27,15 +27,16 @@ def generate_extended_privkey(rootseed):
 		fingerprint = b'\x00\x00\x00\x00'
 		child_num = b'\x00\x00\x00\x00'
 
-		#print(f'Master key: {hexlify(secret_key)}')
+		print(f'Master key: {hexlify(secret_key)}')
 		#print(f'Chain code: {hexlify(chain_code)}\n')
 		
 		xprv = version + depth + fingerprint + child_num + chain_code + b'\x00' + secret_key
 		
+		print(len(secret_key))
 		return b58encode_check(xprv)
 	except ValueError as ve:
 		print(f'Error: {ve}')
 
 
 if __name__ == "__main__":
-	generate_extended_privkey(unhexlify('a83930217f6671af4a1a1bcef98f41c52d0ea70bed8d92b7fbdb8d531efa6d312fe2e2a15acf743387072bdace60cba2846449527f1f8f793fc6abd7d854ae8d'))
+	print(generate_extended_privkey(unhexlify('a83930217f6671af4a1a1bcef98f41c52d0ea70bed8d92b7fbdb8d531efa6d312fe2e2a15acf743387072bdace60cba2846449527f1f8f793fc6abd7d854ae8d')))
--- src/secp256k1.py
+++ src/secp256k1.py
@@ -1,15 +1,48 @@
+from binascii import unhexlify
+
 class secp256k1():
 	# This class represents the secp256k1 elliptic curve: y^2 = x^3 + b (mod p)
 	# Parameters sourced from https://en.bitcoin.it/wiki/Secp256k1
 	def __init__(self):
-		self.p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
-		self.b = 0x0000000000000000000000000000000000000000000000000000000000000007
 		self.x = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
 		self.y = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
 		self.n = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+	
+	def point_add(self, P, Q):
+		# compute P + Q = R
+		delta = ((Q[1] - P[1]) // (Q[0] - P[0])) % self.n
+		x_r = ((delta**2) - P[0] - Q[0]) % self.n
+		y_r = (delta * (P[0] - x_r) - P[1]) % self.n
+		return (x_r, y_r)
 
-	def point_mult(self, p):
-		# FIXME: perform EC point addition with secp256k1 parameters
-		K = 0
-		for i in range(p):
-			K += (self.b) % self.n
+	def point_double(self, P):
+		delta = ((3 * P[0]**2) // (2 * P[1])) % self.n
+		x_r = ((delta**2) - P[0] - self.x) % self.n
+		y_r = (delta * (P[0] - x_r) - P[1]) % self.n
+		return (x_r, y_r)
+
+	def point_mult(self, privkey):
+		# perform EC point addition with secp256k1 parameters K = kP
+		P = (self.x, self.y)
+		K = (0,0)
+		xprv = int(privkey.hex(), 16) 
+		for i in range(len(privkey)*8):
+			if ((xprv >> i) & 0x01) == 0x01:
+				K = self.point_add(K, P)
+			P = self.point_double(P)
+		return K
+
+def trial():
+	# run commands
+	s = secp256k1()
+	pubkey = s.point_mult(unhexlify('e8f32e723decf4051aefac8e2c93c9c5b214313817cdb01a1494b917c8436b35'))
+	eoo = pubkey[1] % 2
+	nx = hex(pubkey[0])[2:]
+	print(nx)
+	if eoo == 1:
+		return '03' + nx
+	return '02' + nx
+
+if __name__ == "__main__":
+	expected_pubkey = '0339a36013301597daef41fbe593a02cc513d0b55527ec2df1050e2e8ff49c85c2'
+	print(f'Expected: {expected_pubkey}\nComputed: {trial()}')
