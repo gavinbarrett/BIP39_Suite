2c0db97cbc0d91f649233ce3aada224ef8655552
Gavin <gavinbrrtt@gmail.com>
2020-12-16 13:19:22 -0800

    Swapped Crypto.Util.number.inverse function for Python 3.8's pow function which supports mod inverse. Updated requirements file to remove tinyec.

--- src/secp256k1.py
+++ src/secp256k1.py
@@ -1,6 +1,3 @@
-from binascii import unhexlify
-from Crypto.Util.number import inverse
-
 class CurvePoint:
 	
 	def __init__(self, x, y):
@@ -16,7 +13,7 @@ class CurvePoint:
 		x2, y2 = other.x, other.y
 		if x1 == x2:
 			return self.double()
-		lam = ((y2 - y1) * inverse(x2 - x1, self.p)) % self.p
+		lam = ((y2 - y1) * pow(x2 - x1, -1, self.p)) % self.p
 		x_r = (pow(lam, 2, self.p) - x1 - x2) % self.p
 		y_r = (lam * (x1 - x_r) - y1) % self.p
 		return CurvePoint(x_r, y_r)
@@ -24,7 +21,7 @@ class CurvePoint:
 	def double(self):
 		''' Double a point P on the secp256k1 curve '''
 		x, y = self.x, self.y
-		lam = (3 * pow(x, 2, self.p) * inverse(2 * y, self.p)) % self.p
+		lam = (3 * pow(x, 2, self.p) * pow(2 * y, -1, self.p)) % self.p
 		x_r = (pow(lam, 2, self.p) - x - self.x) % self.p
 		y_r = (lam * (x - x_r) - y) % self.p
 		return CurvePoint(x_r, y_r)
