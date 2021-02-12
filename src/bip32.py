d2b4551f714201c630638bdef17838ab657fc9e2
Gavin <gavinbrrtt@gmail.com>
2021-02-12 14:40:11 -0800

    Added checks to the child keypair depth and index parameters.

--- src/bip32.py
+++ src/bip32.py
@@ -2,7 +2,7 @@ import json
 import hmac
 import hashlib
 import struct
-from sys import byteorder
+from sys import byteorder, exit
 from binascii import hexlify, unhexlify
 from hashlib import pbkdf2_hmac, sha256, sha512
 from bip39 import bip39, generate_rootseed
@@ -57,8 +57,10 @@ def point(prv_key):
 def compress_pubkey(pubkey):
 	# encode the y coordinte in the first byte of the public key
 	if pubkey.y & 1:
-		return unhexlify('03' + hex(pubkey.x)[2:])
-	return unhexlify('02' + hex(pubkey.x)[2:])
+	#	print(f'Pubkey.x {pubkey.x}\nHex: {hex(pubkey.x)}\nStripped: {hex(pubkey.x)[2:]}')
+		return b'\x03' + pubkey.x.to_bytes(32, endianness)
+		#return unhexlify('03' + hex(pubkey.x)[2:])
+	return b'\x02' + pubkey.x.to_bytes(32, endianness)
 
 def extract_prv(prv):
 	''' Extracts a private key and the chain code from an extended private key '''
@@ -199,30 +201,46 @@ def generate_child_keypair(xprv, xpub, depth, index):
 	return child_prv, child_pub
 
 def generate_keypath(rootkey, path):
-	# FIXME: refactor
+	''' Generate a BIP wallet chain along a given path '''
+	# decode the chain's path
 	path_indices = decode_path(path)
-	# Derive the key pairs along the given path
 	keypairs = []
-	for idx, index in enumerate(path_indices):
+	for depth, index in enumerate(path_indices):
 		if index == "m":
 			# Generate the master extended key pair
 			xprv, xpub = generate_master_extended_keypair(unhexlify(rootkey))
 		else:
-			# Generate a child extended key pair
-			xprv, xpub = generate_child_keypair(xprv, xpub, idx.to_bytes(1, endianness), index)
+			try:
+				# ensure that key index and depth variables do not overflow
+				if not (0x00 <= depth <= 0xff):
+					raise ValueError(f'Invalid key depth {depth}')
+				if not (0x00 <= index <= 0xffffffff):
+					raise ValueError(f'Invalid key index {index}')
+				# Generate a child extended key pair
+				xprv, xpub = generate_child_keypair(xprv, xpub, depth.to_bytes(1, endianness), index)
+			except ValueError as err:
+				print(f'Error deriving child key: {err}.')
+				return None
 		keypairs.append({"prv": xprv, "pub": xpub})
 	return keypairs
 
 if __name__ == "__main__":
 	''' Deriving m/0'/1  for `000102030405060708090a0b0c0d0e0f` root seed '''
-	rootseed = "000102030405060708090a0b0c0d0e0f"
+	rootseed = "fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542"
 	# Generate the first address
-	path = "m/0'/1/2'/2/1000000000/0"
+	path = "m/0/2147483647'/1/2147483646'/2"
 	# generate keys along given path
 	keypairs = generate_keypath(rootseed, path)
+	if keypairs:
+		for k in keypairs:
+			print(k)
+	else:
+		exit(1)
+	'''
 	#print(f'Pairs:\n{keypairs}')
 	pair = keypairs[-1]["pub"]
 	# extract compressed public key
 	extracted_pub = extract_pub(pair)
 	address = generate_legacy_address(extracted_pub)
 	print(f'Address: {address}')
+	'''
