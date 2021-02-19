a51b02b0bf50977faff00e75ac5ae0df46822208
Gavin <gavinbrrtt@gmail.com>
2021-02-19 11:03:09 -0800

    Added SideBar to PrivacyPolicy page.

--- static/ui/components/PrivacyPolicy.tsx
+++ static/ui/components/PrivacyPolicy.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
+import { SideBar } from './SideBar';
 import './sass/PrivacyPolicy.scss';
 
 export const PrivacyPolicy = () => {
-	return (<div className="privacy-policy">
+	return (<div className="privacy-app">
+	<SideBar/>
+	<div className="privacy-policy">
 		{"This is the privacy policy"}
-	</div>);
+	</div></div>);
 }
